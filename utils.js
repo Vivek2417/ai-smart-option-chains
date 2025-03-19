35f5e2f07b12e444d5bfd4a51810031ae90d49e5
Vivek2417 <vivekpithadiya15@gmail.com>
2025-03-20 02:10:17 +0530

    Delete utils.js

--- utils.js
+++
@@ -1,8 +0,0 @@
-export async function fetchOptionsChain() {
-    // Mock API response (Replace with real API URL)
-    return [
-      { strike: 18000, ivRank: 25, oiChange: 1200, aiSignal: "BUY" },
-      { strike: 18200, ivRank: 32, oiChange: -800, aiSignal: "SELL" },
-    ];
-  }
-  
